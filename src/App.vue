feccbe44b456f836b7d0fed6e1c4b31055b5204f
ahmadridwan.id <ahmadridwan.3107@gmail.com>
2021-05-28 17:44:19 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,211 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div >
+      <div ref="pdf" class="invoice-box">
+        <table>
+          <tr class="top">
+            <td colspan="2">
+              <table>
+                <tr>
+                  <td class="title">
+                    <img
+                      src="./assets/logo.png"
+                      alt="Company logo"
+                      style="width: 100%; max-width: 200px"
+                    />
+                  </td>
+
+                  <td>
+                    Invoice #: 123<br />
+                    Created: January 1, 2015<br />
+                    Due: February 1, 2015
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+
+          <tr class="information">
+            <td colspan="2">
+              <table>
+                <tr>
+                  <td>
+                    Sparksuite, Inc.<br />
+                    12345 Sunny Road<br />
+                    Sunnyville, TX 12345
+                  </td>
+
+                  <td>
+                    Acme Corp.<br />
+                    John Doe<br />
+                    john@example.com
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+
+          <tr class="heading">
+            <td>Payment Method</td>
+
+            <td>Check #</td>
+          </tr>
+
+          <tr class="details">
+            <td>Check</td>
+
+            <td>1000</td>
+          </tr>
+
+          <tr class="heading">
+            <td>Item</td>
+
+            <td>Price</td>
+          </tr>
+
+          <tr class="item">
+            <td>Website design</td>
+
+            <td>$300.00</td>
+          </tr>
+
+          <tr class="item">
+            <td>Hosting (3 months)</td>
+
+            <td>$75.00</td>
+          </tr>
+
+          <tr class="item last">
+            <td>Domain name (1 year)</td>
+
+            <td>$10.00</td>
+          </tr>
+
+          <tr class="total">
+            <td></td>
+
+            <td>Total: $385.00</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <button @click="download">Download PDF</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { jsPDF } from 'jspdf';
+import * as html2canvas from 'html2canvas';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    download() {
+      return new Promise((resolve, reject) => {
+        let windowHeight = window.innerHeight;
+        let windowWidth = window.innerWidth;
+
+        let pdf = new jsPDF();
+
+        let canvasElement = document.createElement('canvas');
+        canvasElement.width = windowWidth;
+        canvasElement.height = windowHeight;
+
+        html2canvas(this.$refs.pdf, {
+          canvas: canvasElement,
+          width: windowWidth,
+          height: windowHeight
+        })
+          .then((canvas) => {
+            const img = canvas.toDataURL('image/jpeg', 1);
+            document.body.appendChild(canvas);
+            pdf.addImage(img, 'JPEG', 10, 10);
+            pdf.save('sample.pdf');
+            resolve();
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.invoice-box {
+  max-width: 720px;
+  border: 1px solid #eee;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  font-size: 16px;
+  line-height: 24px;
+  font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+  color: #555;
+}
+
+.invoice-box table {
+  width: 100%;
+  line-height: inherit;
+  text-align: left;
+  border-collapse: collapse;
+}
+
+.invoice-box table td {
+  padding: 5px;
+  vertical-align: top;
+}
+
+.invoice-box table tr td:nth-child(2) {
+  text-align: right;
+}
+
+.invoice-box table tr.top table td {
+  padding-bottom: 20px;
+}
+
+.invoice-box table tr.top table td.title {
+  font-size: 45px;
+  line-height: 45px;
+  color: #333;
+}
+
+.invoice-box table tr.information table td {
+  padding-bottom: 40px;
+}
+
+.invoice-box table tr.heading td {
+  background: #eee;
+  border-bottom: 1px solid #ddd;
+  font-weight: bold;
+}
+
+.invoice-box table tr.details td {
+  padding-bottom: 20px;
+}
+
+.invoice-box table tr.item td {
+  border-bottom: 1px solid #eee;
+}
+
+.invoice-box table tr.item.last td {
+  border-bottom: none;
+}
+
+.invoice-box table tr.total td:nth-child(2) {
+  border-top: 2px solid #eee;
+  font-weight: bold;
+}
+
+@media only screen and (max-width: 600px) {
+  .invoice-box table tr.top table td {
+    width: 100%;
+    display: block;
+    text-align: center;
+  }
+
+  .invoice-box table tr.information table td {
+    width: 100%;
+    display: block;
+    text-align: center;
+  }
 }
 </style>
